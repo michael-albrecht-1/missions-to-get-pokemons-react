1626857ab726f4c354f9d0b35488c654be11d657
michael.albrecht <albrecht.michael@hotmail.com>
2023-03-22 22:04:21 +0100

    install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { ISearchAllPokemons } from './pokemon/usecases/ISearchAllPokemons';
 import { PokeApiPokemonLoader } from './pokemon/adapters/secondaries/real/REST-poke-api/PokeApiPokemon.loader';
 import { PokemonLoader } from './pokemon/usecases/loaders/PokemonLoader';
 import { ObservableRESTClient } from './config/clients/observable.RESTClient';
+import Grid from '@mui/material/Unstable_Grid2';
+import { Box } from '@mui/material';
 
 function App() {
   const pokemonsSouce: PokemonLoader = new PokeApiPokemonLoader(
@@ -23,12 +25,15 @@ function App() {
   return (
     <div className="App">
       poke app
-      <ul>
-        {pokemons.map((p: Pokemon) => (
-          <PokemonCard key={p.snapshot().number} pokemon={p} />
-        ))}
-      </ul>
-    </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          {pokemons.map((p: Pokemon) => (
+            <Grid xs={12} md={6} lg={4}>
+              <PokemonCard key={p.snapshot().number} pokemon={p} />
+            </Grid>
+          ))}
+        </Grid></Box>
+    </div >
   )
 }
 
--- src/pokemon/adapters/primaries/pokemon-card/pokemon-card.tsx
+++ src/pokemon/adapters/primaries/pokemon-card/pokemon-card.tsx
@@ -1,8 +1,37 @@
 import { Pokemon } from "../../../domain/entity/pokemon";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
-function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
-    return <div className="PokemonCard">{pokemon.snapshot().name}</div>;
+
+export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 260, maxWidth: 300, objectFit: 'contain' }}
+                image={getImgSrc(pokemon.snapshot().number)}
+                title={pokemon.snapshot().name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {pokemon.snapshot().name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
 
-export default PokemonCard;
+const getImgSrc = (pokemonNumber: string): string => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonNumber}.svg`;
+}
