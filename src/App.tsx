f45e7551e39cb6571888188bfe6b87a55f2d51f6
michael.albrecht <albrecht.michael@hotmail.com>
2023-03-23 20:40:35 +0100

    wip nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,31 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Pokemon } from './pokemon/domain/entity/pokemon';
-import PokemonCard from './pokemon/adapters/primaries/pokemon-card/pokemon-card';
-import { ISearchAllPokemons } from './pokemon/usecases/ISearchAllPokemons';
-import { PokeApiPokemonLoader } from './pokemon/adapters/secondaries/real/REST-poke-api/PokeApiPokemon.loader';
-import { PokemonLoader } from './pokemon/usecases/loaders/PokemonLoader';
-import { ObservableRESTClient } from './config/clients/observable.RESTClient';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Box } from '@mui/material';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Pokemon } from "./pokemon/domain/entity/pokemon";
+import PokemonCard from "./pokemon/adapters/primaries/pokemon-card/pokemon-card";
+import { ISearchAllPokemons } from "./pokemon/usecases/ISearchAllPokemons";
+import { PokeApiPokemonLoader } from "./pokemon/adapters/secondaries/real/REST-poke-api/PokeApiPokemon.loader";
+import { PokemonLoader } from "./pokemon/usecases/loaders/PokemonLoader";
+import { ObservableRESTClient } from "./config/clients/observable.RESTClient";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Box } from "@mui/material";
+import Pokemons from "./pokemon/adapters/primaries/pokemons/pokemons";
+import Navbar from "./core/navbar/navbar";
 
 function App() {
-  const pokemonsSouce: PokemonLoader = new PokeApiPokemonLoader(
-    new ObservableRESTClient()
-  );
-  const iSearchAllPokemons = new ISearchAllPokemons(pokemonsSouce);
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    iSearchAllPokemons.execute().subscribe((pokemons: Pokemon[]) => {
-      setPokemons(pokemons);
-    });
-  }, []);
-
   return (
     <div className="App">
-      poke app
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          {pokemons.map((p: Pokemon) => (
-            <Grid xs={12} sm={6} md={4} lg={3} xl={2}>
-              <PokemonCard key={p.snapshot().number} pokemon={p} />
-            </Grid>
-          ))}
-        </Grid></Box>
-    </div >
-  )
+          <Grid xs={12}>
+            <Navbar />
+          </Grid>
+          <Grid xs={12}>
+            <Pokemons />
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pokemon/adapters/primaries/pokemons/pokemons.tsx
@@ -0,0 +1,35 @@
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { useEffect, useState } from "react";
+import { ObservableRESTClient } from "../../../../config/clients/observable.RESTClient";
+import { Pokemon } from "../../../domain/entity/pokemon";
+import { ISearchAllPokemons } from "../../../usecases/ISearchAllPokemons";
+import { PokemonLoader } from "../../../usecases/loaders/PokemonLoader";
+import { PokeApiPokemonLoader } from "../../secondaries/real/REST-poke-api/PokeApiPokemon.loader";
+import PokemonCard from "../pokemon-card/pokemon-card";
+
+export default function Pokemons() {
+    const pokemonsSouce: PokemonLoader = new PokeApiPokemonLoader(
+        new ObservableRESTClient()
+    );
+    const iSearchAllPokemons = new ISearchAllPokemons(pokemonsSouce);
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        iSearchAllPokemons.execute().subscribe((pokemons: Pokemon[]) => {
+            setPokemons(pokemons);
+        });
+    }, []);
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                {pokemons.map((p: Pokemon) => (
+                    <Grid key={p.snapshot().number} xs={12} sm={6} md={4} lg={3} xl={2}>
+                        <PokemonCard pokemon={p} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    );
+}
